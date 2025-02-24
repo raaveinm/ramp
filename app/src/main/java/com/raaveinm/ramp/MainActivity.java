9ed3b738b64a0b7a4a1fc93dedbf5070318017ea
raaveinm <dulovkir72@gmail.com>
2025-02-25 00:42:23 +0300

    Basic design for main activity created onPlay.java, which stands for player functions

--- app/src/main/java/com/raaveinm/ramp/MainActivity.java
+++ app/src/main/java/com/raaveinm/ramp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.raaveinm.ramp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.raaveinm.ramp.onPlay;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton ButtonPlayPause = findViewById(R.id.ButtonPlayPause);
+    ImageButton ButtonNext = findViewById(R.id.ButtonNext);
+    ImageButton ButtonPrevious = findViewById(R.id.ButtonPrevious);
+    ImageButton SortButton = findViewById(R.id.SortButton);
+    ImageButton ButtonMenu = findViewById(R.id.ButtonMenu);
+
+    Integer PlayPauseStatus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ButtonPlayPause.setOnClickListener(v -> {
+            if (PlayPauseStatus == 0){
+                onPlay.MediaSessionCallback.onPlay();
+                PlayPauseStatus = 1;
+
+            }
+            else{
+                onPlay.MediaSessionCallback.onPause();
+                PlayPauseStatus = 0;
+            }
         });
     }
 }
